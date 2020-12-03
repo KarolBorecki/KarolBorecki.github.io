484de2464204732b17db75ff710f4b86c9bf8406
Karol Borecki <karol.borecki@onet.eu>
2020-12-03 11:38:42 +0100

    Cursor has been hidden.

--- sketch.js
+++ sketch.js
@@ -41,6 +41,7 @@ function draw() {
     return;
   }
   mouseY = 0;
+  noCursor();
   player.display();
   textSize(canvasWidth/20);
   text(points, canvasWidth - canvasWidth/10, 0, canvasWidth/10, canvasWidth/10);
