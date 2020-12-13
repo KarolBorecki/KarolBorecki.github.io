902656b1397b6290faa2778505e3c2f15772fac3
Karol Borecki <karol.borecki@onet.eu>
2020-12-13 15:42:35 +0100

    fixed z positioning

--- sketch.js
+++ sketch.js
@@ -119,8 +119,8 @@ function draw() {
     image(underline, canvasWidth/4, canvasHeight/4.5, canvasWidth/2, canvasWidth/16);
 
     for(var i = 0; i<5; i++){
-      if(i<4)image(okImg, (canvasWidth/6 + canvasWidth/6*(i)), canvasHeight/2-canvasWidth/12, canvasWidth/15, canvasWidth/15);
       image(ingredientsImg[playersIngredients[choosenPizza][i]], (canvasWidth/12 + canvasWidth/6*i), canvasHeight/2-canvasWidth/10, canvasWidth/6, canvasWidth/6);
+      if(i<4)image(okImg, (canvasWidth/6 + canvasWidth/6*(i)), canvasHeight/2-canvasWidth/12, canvasWidth/15, canvasWidth/15);
     }
 
     playBtn.display();
