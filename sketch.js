cb2a87be0fe35c4ebb0dc145529ff6a93a85de83
Karol Borecki <karol.borecki@onet.eu>
2020-12-13 14:33:33 +0100

    fixed points position

--- sketch.js
+++ sketch.js
@@ -131,8 +131,8 @@ function draw() {
 
     textSize(canvasWidth/16);
     textAlign(CENTER);
-    text(points, canvasWidth - canvasWidth/9, 0, canvasWidth/7, canvasWidth/7);
-    image(pointsFrame, canvasWidth - canvasWidth/7, canvasWidth/14, canvasWidth/7, canvasWidth/7);
+    text(points, canvasWidth - canvasWidth/9, canvasWidth/28, canvasWidth/7, canvasWidth/7);
+    image(pointsFrame, canvasWidth - canvasWidth/7, 0, canvasWidth/7, canvasWidth/7);
 
     time += 25;
   }else{
