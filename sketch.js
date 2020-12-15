5b1f094c6c8b2ce723a71beff7556a76854fee1e
Karol Borecki <karol.borecki@onet.eu>
2020-12-15 13:14:00 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -273,7 +273,7 @@ class Player {
     if(gameStatus == 2){
       this.x = mouseX - this.width/2;
       image(this.img, this.x, this.startPosY, this.width, this.height);
-      var val = (25*Math.floor(timeToNextIngredient/Math.sqrt(timeDivider)));
+      var val = (25*timeToNextIngredient/Math.sqrt(timeDivider));
       console.log(val);
       if(time%val == 0) this.fallIngredient();
       if(time%(25*Math.floor(timeToNextBadIngredient/Math.sqrt(timeDivider))) == 0) this.fallBadIngredient();
