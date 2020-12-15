4572c93b2d7ba033a6ee6fbaffccf4081e684575
Karol Borecki <karol.borecki@onet.eu>
2020-12-15 17:48:52 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -276,7 +276,7 @@ class Player {
       this.x = mouseX - this.width/2;
       image(this.img, this.x, this.startPosY, this.width, this.height);
 
-      var val = timeToNextIngredient*Math.sqrt(startTimeToNextIngredient);
+      var val = timeToNextIngredient/Math.sqrt(startTimeToNextIngredient);
       console.log(val);
 
       if(time%timeToNextIngredient == 0) this.fallIngredient();
