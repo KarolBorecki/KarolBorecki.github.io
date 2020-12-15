a923b94ed941ea2a0a844888ff6408e26b8d0e7e
Karol Borecki <karol.borecki@onet.eu>
2020-12-15 12:53:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,8 +15,8 @@ var minSpeed = 4;
 var startMaxSpeed = 1;
 var startMinSpeed = 1;
 
-var timeToNextIngredient = 4000;
-var timeToNextBadIngredient = 12000;
+var timeToNextIngredient = 1400;
+var timeToNextBadIngredient = 8000;
 var timeDivider = 1;
 var ingredientsCount = 5;
 
