ccf4d1d933c42e816d662a9b1fc23aeabd1e90bd
Karol Borecki <karol.borecki@onet.eu>
2020-12-17 22:20:05 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -245,9 +245,13 @@ function instructionView(){
       if(i<4)image(okImg, (canvasWidth/6 + canvasWidth/6*i), canvasHeight*7/16 + ((i%2==0) ? 0 : -canvasWidth/14), canvasWidth/15, canvasWidth/15);
     }
     image(xImg, (canvasWidth/12 + canvasWidth/6*4), canvasHeight*7/16 - canvasWidth/28, canvasWidth/6, canvasWidth/6);
-*/
-    image(ingredientsImg[playersIngredients[choosenPizza][0]], canvasWidth/6, canvasHeight/2, canvasWidth/3, canvasWidth/3);
-    image(ingredientsImg[playersIngredients[choosenPizza][1]], canvasWidth/6+canvasWidth/3, canvasHeight/2, canvasWidth/3, canvasWidth/3);
+    */
+    image(ingredientsImg[playersIngredients[choosenPizza][0]], canvasWidth/6, canvasHeight/3, canvasWidth/3, canvasWidth/3);
+    image(ingredientsImg[playersIngredients[choosenPizza][1]], canvasWidth/6+canvasWidth/3, canvasHeight/3, canvasWidth/3, canvasWidth/3);
+
+    image(ingredientsImg[playersIngredients[choosenPizza][2]], canvasWidth/6, canvasHeight/2, canvasWidth/3, canvasWidth/3);
+    image(ingredientsImg[playersIngredients[choosenPizza][3]], canvasWidth/6+canvasWidth/3, canvasHeight/2, canvasWidth/3, canvasWidth/3);
+    image(ingredientsImg[playersIngredients[choosenPizza][4]], canvasWidth/6 + canvasWidth*2/3, canvasHeight/2, canvasWidth/3, canvasWidth/3);
 
     playGIF.display(canvasWidth/3, canvasHeight-canvasWidth/3, canvasWidth/3, canvasWidth/3);
   }
