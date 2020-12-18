1fdab7d3c350f4666137699d97f05f7f96b45265
Karol Borecki <karol.borecki@onet.eu>
2020-12-18 12:56:37 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -191,7 +191,7 @@ function menuView(){
   if(!arrowLeft.over() && !arrowRight.over()) arrowMove = 0;
 
   if(!isVertical){
-    textSize(canvasWidth/34);
+    textSize(wM*10);
     image(caption, wM*10, hM*4, wM*10, wM*10/4);
 
     image(playersImg[choosenPizza], wM*11.5, hM*8, wM*7, wM*4);
